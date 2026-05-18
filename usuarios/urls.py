f5fa729b2713f8c39b4579de41598d4de23ef882
Facundo Carbone <carbonefacu78@gmail.com>
2026-05-18 17:52:21 -0300

    agrego la hu de ver perfil

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),          # <-- Ahora es /login/
     path('register/', views.register_view, name='register'), # /register/
     path('logout/', views.logout_view, name='logout'),
+    path('perfil/', views.perfil_view, name='perfil'),
 ]
