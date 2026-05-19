03b1f0438ad92a0e384261d967fb823630c9613a
Facundo Carbone <carbonefacu78@gmail.com>
2026-05-18 21:07:30 -0300

    ver perfil y modificar perfil completas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'), # /register/
     path('logout/', views.logout_view, name='logout'),
     path('perfil/', views.perfil_view, name='perfil'),
+    path('perfil/editar',views.editar_perfil_view,name='editar_perfil'),
 ]
