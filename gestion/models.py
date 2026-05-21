f597335277ac02801ed69f84426087b89b8446de
Luca Dedich <lucadedich@gmail.com>
2026-05-21 11:20:59 -0300

    Fix: Corrección en alta de profesores, vinculación con usuario y campos faltantes

--- gestion/models.py
+++ gestion/models.py
@@ -8,10 +8,26 @@ class Actividad(models.Model):
     def __str__(self):
         return self.nombre
 
-class Profesor(models.Model):
+"""
+    class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     especialidad = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+"""
+        
+class Profesor(models.Model):
+    # Relación directa con el Usuario que creamos en la otra app
+    usuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    especialidad = models.CharField(max_length=100, blank=True)
+    dni = models.CharField(max_length=20, unique=True, null=True, blank=True) # Agregamos el DNI
+    telefono = models.CharField(max_length=30, blank=True, null=True)
+    correo = models.EmailField(blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
